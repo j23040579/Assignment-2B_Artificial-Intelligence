42f480f7bdb8383cba8704f6103795ed56c322e1
j23040579 <j23040579@student.newinti.edu.my>
2026-06-12 10:56:02 +0800

    Changed the path of the models

--- RNN.py
+++ RNN.py
@@ -33,9 +33,9 @@ SEQ_LENGTH        = 3
 EPOCHS            = 50
 BATCH_SIZE        = 32
 TEST_SPLIT        = 0.2
-MODEL_SAVE_PATH   = 'machine_learning/trained_models/models/rnn_model.keras'
-SCALER_SAVE_PATH  = 'machine_learning/trained_models/models/rnn_scaler.pkl'
-ENCODER_SAVE_PATH = 'machine_learning/trained_models/models/rnn_label_encoder.pkl'
+MODEL_SAVE_PATH   = 'models/rnn_model.keras'
+SCALER_SAVE_PATH  = 'models/rnn_scaler.pkl'
+ENCODER_SAVE_PATH = 'models/rnn_label_encoder.pkl'
 
 # All artefacts that must be present to skip training
 REQUIRED_ARTEFACTS = [MODEL_SAVE_PATH, SCALER_SAVE_PATH, ENCODER_SAVE_PATH]
@@ -245,7 +245,7 @@ def train_and_save() -> tuple:
         verbose=1
     )
 
-    os.makedirs('machine_learning/trained_models/models', exist_ok=True)
+    os.makedirs('models', exist_ok=True)
     model.save(MODEL_SAVE_PATH)
     print(f"\n Model saved  → {MODEL_SAVE_PATH}")
 
--- machine_learning/trained_models/README.md
+++
@@ -1,3 +0,0 @@
-# Purpose of this folder
-This folder contains files of models that you have trained such as "file_name.tensor" & "file_name.pkt".
-Usually after you trained your models from your code, it will save the file name as "file_name.tensor".
